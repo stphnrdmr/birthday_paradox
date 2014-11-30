732d54ac8545a85def58dc235a5b107989aac981
Stephan Rodemeier <stephan.rodemeier@gmail.com>
2014-11-30 21:58:11 +0100

    Remove json and clean up routes

--- app/views/people/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@people) do |person|
-  json.extract! person, :id, :birthday
-  json.url person_url(person, format: :json)
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'people#new'
-  get 'result' => 'people#result'
   get 'adm0n' => 'people#clear'
   resources :people, :except => [:edit, :update, :destroy]
 end
